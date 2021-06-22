5604c70e0f38b7d1c65505f2647d143572ef29f4
keerthi929 <69851175+keerthi929@users.noreply.github.com>
2021-06-22 12:55:30 +0530

    Add files via upload

---
+++ task8  outputs.py
@@ -0,0 +1,11 @@
+
+#task 8 outputs
+
+list in ascending order: [9, 10, 15, 23, 35, 46, 67, 78, 80]
+
+['core', 'functions', 'language', 'programming']
+
+['core', 'functions', 'language', 'programming']
+
+Enter String:python
+pythoo
