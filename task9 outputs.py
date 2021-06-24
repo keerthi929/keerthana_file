7111aef68e88cee00eb324972250e1ebe3973a75
keerthi929 <69851175+keerthi929@users.noreply.github.com>
2021-06-24 14:09:47 +0530

    Add files via upload

---
+++ task9 outputs.py
@@ -0,0 +1,86 @@
+1)output
+original list: [34, 56, 78, 32, 98, 23, 45]
+The new list is: [36, 58, 80, 34, 100, 25, 47]
+
+2)
+54321
+4321
+321
+21
+1
+
+3)
+Enter the value of n: 8
+Fibonacci Series:
+0
+1
+1
+2
+3
+5
+8
+13
+
+4)
+enter a number :87
+855
+given number is not armstrong
+
+5)
+Enter a number:9
+9 x 1 = 9
+9 x 2 = 18
+9 x 3 = 27
+9 x 4 = 36
+9 x 5 = 45
+9 x 6 = 54
+9 x 7 = 63
+9 x 8 = 72
+9 x 9 = 81
+9 x 10 = 90
+
+6)
+Enter no of days:6789
+Age is:  18.6
+
+7)
+Enter a number :87
+87.0 is positive Number
+
+8)
+0.8660254037844386
+1.7320508075688767
+0.5000000000000001
+
+9)
+Select operation.
+1.Add
+2.Subtract
+3.Multiply
+4.Divide
+Enter choice(1/2/3/4/): 1
+Enter first number: 34
+Enter second number: 45
+34.0 + 45.0 = 79.0
+
+Do you want to continue enter your choice yes/no  : yes
+
+Enter choice(1/2/3/4/): 2
+Enter first number: 23
+Enter second number: 4
+23.0 - 4.0 = 19.0
+
+Do you want to continue enter your choice yes/no  : yes
+
+Enter choice(1/2/3/4/): 3
+Enter first number: 25
+Enter second number: 5
+25.0 * 5.0 = 125.0
+
+Do you want to continue enter your choice yes/no  : yes
+
+Enter choice(1/2/3/4/): 4
+Enter first number: 625
+Enter second number: 5
+625.0 / 5.0 = 125.0
+Do you want to continue enter your choice yes/no  : no
